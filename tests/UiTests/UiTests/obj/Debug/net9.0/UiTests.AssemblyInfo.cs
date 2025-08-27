27f388150ad9666c928f21febcd1c51077169b9f
kvssandeep6105 <saisandeep6105@gmail.com>
2025-08-27 21:33:21 +0530

    Publsig branch

--- tests/UiTests/UiTests/obj/Debug/net9.0/UiTests.AssemblyInfo.cs
+++ tests/UiTests/UiTests/obj/Debug/net9.0/UiTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UiTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e7c278bef6d3d4c97c9350b6ee95cc3cdac024d")]
 [assembly: System.Reflection.AssemblyProductAttribute("UiTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UiTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
